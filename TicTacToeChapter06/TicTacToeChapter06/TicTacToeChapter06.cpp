0571f7ab47539a5a11d2716b581505bbc76267e9
YiYANZtY <yiyanzty@gmail.com>
2020-11-09 22:46:49 +0800

    TieTacToe程序增添几个函数实现。

--- TicTacToeChapter06/TicTacToeChapter06/TicTacToeChapter06.cpp
+++ TicTacToeChapter06/TicTacToeChapter06/TicTacToeChapter06.cpp
@@ -36,4 +36,92 @@ int main()
 	char human = humanPiece();
 	char computer = opponent(human);
 	char turn = X;
+	displayBoard(board);
+	while (winner(board) == NO_ONE)
+	{
+		if (turn == human)
+		{
+			move = humanMove(board, human);
+			board[move] = human;
+		}
+		else
+		{
+			move = computerMove(board, computer);
+			board[move] = computer;
+		}
+		displayBoard(board);
+		turn = opponent(turn);
+	}
+	announceWinner(winner(board), computer, human);
+	return 0;
+}
+
+void instructions()
+{
+	cout << "欢迎来到井字棋游戏！\n";
+	cout << "输入0—8来控制你落子的位置，对应棋盘的位置如图所示：\n";
+	cout << "0 | 1 | 2\n";
+	cout << "---------\n";
+	cout << "3 | 4 | 5\n";
+	cout << "---------\n";
+	cout << "6 | 7 | 8\n";
+	cout << "对弈即将开始！\n";
+}
+
+char askYesNo(string question) 
+{
+	char response;
+	do
+	{
+		cout << question << " (y/n):";
+		cin >> response;
+	} while (response != 'y' && response != 'n');
+	return response;
+}
+
+int askNumber(string question, int high, int low = 0)
+{
+	int number;
+	do
+	{
+		cout << question << " (" << low << " - " << high << "): ";
+		cin >> number;
+	} while (number >= high || number <= low);
+	return number;
+}
+
+char humanPiece()
+{
+	char goFirst = askYesNo("你想先走吗？");
+	if (goFirst == 'y')
+	{
+		cout << "\n那你先走。\n";
+		return X;
+	}
+	else
+	{
+		cout << "\n那我先走了。\n";
+		return O;
+	}
+}
+
+char opponent(char piece)
+{
+	if (piece == X)
+	{
+		return O;
+	}
+	else
+	{
+		return X;
+	}
+}
+
+void displayBoard(const vector<string>& board)
+{
+	cout << "\n\t" << board[0] << " | " << board[1] << " | " << board[2];
+	cout << "\n\t" << "---------";
+	cout << "\n\t" << board[3] << " | " << board[4] << " | " << board[5];
+	cout << "\n\t" << "---------";
+	cout << "\n\t" << board[6] << " | " << board[7] << " | " << board[8] << endl;
 }
